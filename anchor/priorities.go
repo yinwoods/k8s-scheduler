8339e797d27735f3b697e3ec33b57fee1bbf2b08
ctyin <ctyin@freewheel.tv>
2018-07-13 00:39:02 +0800

    gofmt

--- anchor/priorities.go
+++ anchor/priorities.go
@@ -3,66 +3,66 @@ package main
 const MaxPriority = 10
 
 func balancedResourceScore(requested, allocatable ResourceUsage) int64 {
-    cFraction := fractionOfCapacity(requested.CPU, allocatable.CPU)
-    mFraction := fractionOfCapacity(requested.Memory, allocatable.Memory)
-    pFraction := fractionOfCapacity(requested.Pod, allocatable.Pod)
+	cFraction := fractionOfCapacity(requested.CPU, allocatable.CPU)
+	mFraction := fractionOfCapacity(requested.Memory, allocatable.Memory)
+	pFraction := fractionOfCapacity(requested.Pod, allocatable.Pod)
 
-    if cFraction >= 1 || mFraction >= 1  || pFraction >= 1{
-        return 0
-    }
+	if cFraction >= 1 || mFraction >= 1 || pFraction >= 1 {
+		return 0
+	}
 
-    return getBalancedResourceScore(cFraction, mFraction, pFraction)
+	return getBalancedResourceScore(cFraction, mFraction, pFraction)
 }
 
 func leastRequestedScore(requested, allocatable ResourceUsage) int64 {
-    cRatio := getLeastRequestedScore(requested.CPU, allocatable.CPU)
-    mRatio := getLeastRequestedScore(requested.Memory, allocatable.Memory)
-    pRatio := getLeastRequestedScore(requested.Pod, allocatable.Pod)
+	cRatio := getLeastRequestedScore(requested.CPU, allocatable.CPU)
+	mRatio := getLeastRequestedScore(requested.Memory, allocatable.Memory)
+	pRatio := getLeastRequestedScore(requested.Pod, allocatable.Pod)
 
-    return (cRatio + mRatio + pRatio) / 3
+	return (cRatio + mRatio + pRatio) / 3
 }
 
 func priorities(pod *Pod, nodes []*Node) (*Node, error) {
 
-    var bestNode *Node
-    nodeScore := make(map[*Node]int64)
+	var bestNode *Node
+	nodeScore := make(map[*Node]int64)
 
-    // 获取所有节点
-    nodeList, err := getNodes()
-    errFatal(err, "failed to get nodes")
+	// 获取所有节点
+	nodeList, err := getNodes()
+	errFatal(err, "failed to get nodes")
 
-    // 获取所有pod
-    podList, err := getPods()
-    errFatal(err, "failed to get pods")
+	// 获取所有pod
+	podList, err := getPods()
+	errFatal(err, "failed to get pods")
 
-    requested := requestedResource(pod)
-    used := usedResource(nodeList, podList)
+	requested := requestedResource(pod)
+	used := usedResource(nodeList, podList)
 
-    for _, node := range nodeList.Items {
-        nodeScore[node] = 0
-    }
+	for _, node := range nodeList.Items {
+		nodeScore[node] = 0
+	}
 
-    for _, node := range nodeList.Items {
+	for _, node := range nodeList.Items {
 
-        allocatable := allocatableResource(node, used)
-        nodeScore[node] += balancedResourceScore(requested, allocatable)
-        printNodeScores(nodeScore)
+		allocatable := allocatableResource(node, used)
+		nodeScore[node] += balancedResourceScore(requested, allocatable)
+		printNodeScores(nodeScore)
 
-        nodeScore[node] += leastRequestedScore(requested, allocatable)
-        printNodeScores(nodeScore)
+		nodeScore[node] += leastRequestedScore(requested, allocatable)
+		printNodeScores(nodeScore)
 
-        nodeScore[node] /= 2
-        printNodeScores(nodeScore)
-    }
+		nodeScore[node] /= 2
+		printNodeScores(nodeScore)
+	}
 
-    printNodeScores(nodeScore)
+	printNodeScores(nodeScore)
 
-    var maxScore int64 = 0
-    for node, score := range nodeScore {
-        if score > maxScore {
-            maxScore = score
-            bestNode = node
-        }
-    }
-    return bestNode, nil
+	var maxScore int64 = 0
+	for node, score := range nodeScore {
+		if score > maxScore {
+			maxScore = score
+			bestNode = node
+		}
+	}
+	return bestNode, nil
 }
